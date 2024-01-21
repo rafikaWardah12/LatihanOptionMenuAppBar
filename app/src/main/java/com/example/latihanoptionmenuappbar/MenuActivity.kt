5d6571944beba2c90cc36ad239051c8b143adf77
rafikawardah <rafika.wardah12@gmail.com>
2024-01-21 16:18:14 +0700

    Make SearchBar

--- app/src/main/java/com/example/latihanoptionmenuappbar/MenuActivity.kt
+++ app/src/main/java/com/example/latihanoptionmenuappbar/MenuActivity.kt
@@ -2,10 +2,27 @@ package com.example.latihanoptionmenuappbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.latihanoptionmenuappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.text = searchView.text
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
+        }
     }
 }
